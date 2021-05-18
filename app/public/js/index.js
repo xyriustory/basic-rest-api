f3a589a0c49517522592a591f5859041ae2258dc
xyriustory <xstory.aki0730.sakks@gmail.com>
2021-05-19 04:43:46 +0900

    新規ユーザー作成ページの作成

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,11 +1,31 @@
 const indexModule = (() => {
-  // 検索ボタンをクリックしたときのイベントリスナー設定
-  document.getElementById('search-btn')
-  .addEventListener('click', () => {
-    return searchModule.searchUsers();
-  });
+  const path = window.location.pathname;
+  switch(path)
+  {
+    case '/':
+      // 検索ボタンをクリックしたときのイベントリスナー設定
+      document.getElementById('search-btn')
+      .addEventListener('click', () => {
+        return searchModule.searchUsers();
+      });
+    
+      // UsersモジュールのfetchAllUsersメソッドを呼び出す
+      return usersModule.fetchAllUsers();
 
-  // UsersモジュールのfetchAllUsersメソッドを呼び出す
-  return usersModule.fetchAllUsers();
+    case '/create.html':
+        document.getElementById('save-btn')
+        .addEventListener('click', () => {
+          return usersModule.createUser();
+        });
+        document.getElementById('cancel-btn')
+        .addEventListener('click', () => {
+          return window.location.href = '/';
+        });
+        break;
+
+    default:
+      break;
+  }
+
 })();
 
