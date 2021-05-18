3823dcc579fc3ac150b475b8e1af9c73ff49c331
xyriustory <xstory.aki0730.sakks@gmail.com>
2021-05-19 05:43:48 +0900

    編集、削除ページの作成

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -13,15 +13,31 @@ const indexModule = (() => {
       return usersModule.fetchAllUsers();
 
     case '/create.html':
-        document.getElementById('save-btn')
-        .addEventListener('click', () => {
-          return usersModule.createUser();
-        });
-        document.getElementById('cancel-btn')
-        .addEventListener('click', () => {
-          return window.location.href = '/';
-        });
-        break;
+      document.getElementById('save-btn')
+      .addEventListener('click', () => {
+        return usersModule.createUser();
+      });
+      document.getElementById('cancel-btn')
+      .addEventListener('click', () => {
+        return window.location.href = '/';
+      });
+      break;
+    
+    case '/edit.html':
+      const uid = window.location.search.split('?uid=')[1];
+      document.getElementById('save-btn')
+      .addEventListener('click', () => {
+        return usersModule.saveUser(uid);
+      });
+      document.getElementById('cancel-btn')
+      .addEventListener('click', () => {
+        return window.location.href = '/';
+      });
+      document.getElementById('delete-btn')
+      .addEventListener('click', () => {
+        return usersModule.deleteUser(uid);
+      });
+      return usersModule.setExistingValue(uid);
 
     default:
       break;
